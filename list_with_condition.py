1919158840cb91df445f3ddc988ceac79faf1df1
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 19:14:35 +0800

    project done

--- list_with_condition.py
+++ list_with_condition.py
@@ -3,7 +3,17 @@
 
 # pseudocode
 
-# createe the function
+# create the function
+def create_new_list(list1, list2):
+    new_list = [x for x in list1 if x % 2 != 0] + [y for y in list2 if y % 2 == 0]
+    return new_list
+
 # provide the list
+list1 = [10, 20, 25, 30, 35]
+list2 = [40, 45, 60, 75, 90]
+
 # print the result
+result = create_new_list(list1, list2)
+print(result)
+
 

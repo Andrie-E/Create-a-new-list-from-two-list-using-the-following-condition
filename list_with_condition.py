53b7207c460efb28006c50a4b354d57e0db024af
Andrie-E <stpaul.martinla@gmail.com>
2024-01-26 19:06:13 +0800

    pseudocode

--- list_with_condition.py
+++ list_with_condition.py
@@ -1,2 +1,9 @@
 # Create a new list from two list using the following condition
 # Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
+
+# pseudocode
+
+# createe the function
+# provide the list
+# print the result
+
